8d5768f7be0b65db1aafeddacf63a2e7470876d7
passelin <pascalasselin@hotmail.com>
2023-12-01 23:00:26 -0500

    day macro

---
+++ src/day1.rs
@@ -0,0 +1,8 @@
+
+pub fn part1(content: String) {
+
+}
+
+pub fn part2(content: String) {
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,25 @@
 mod day1;
+mod macros;
+
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Optional name to operate on
+    day: u8,
+    part: u8,
+}
+
+fn get_content(day: u8, part: u8) -> String {
+    format!("({day}, {part})")
+}
+
+make_days_map!(run_day, get_content, {
+    1 => day1
+});
 
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    run_day(args.day, args.part);
 }
