e3ebe2624e8ee7d0922a2def9681ef1b75942a52
passelin <pascalasselin@hotmail.com>
2023-12-11 00:48:43 -0500

    day 7 part 2

--- src/day7.rs
+++ src/day7.rs
@@ -1,11 +1,11 @@
 use std::cmp::Ordering;
-use std::collections::{HashMap, HashSet};
+use std::collections::{HashMap};
 
 // PARSING -------------------------------------
 
 type Card = usize;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Hand {
     cards: Vec<Card>
 }
@@ -16,25 +16,22 @@ struct Play {
     bid: u64
 }
 
-fn parse(content: &str) -> Vec<Play> {
-    //                          0    1    2    3    4    5    6    7    8    9   10   11    12   13
-    const CARDS: [char; 14] = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'];
-
-    fn parse_hand(hand: &str) -> Hand {
+fn parse(content: &str, deck: &'static [char]) -> Vec<Play> {
+    let parse_hand = |hand: &str| -> Hand {
         assert_eq!(5, hand.len());
         let cards = hand.chars()
-            .map(|c| CARDS.iter().position(|cc| *cc == c).unwrap())
+            .map(|c| deck.iter().position(|cc| *cc == c).unwrap())
             .collect();
         Hand { cards }
-    }
-    fn parse_play(c: &str) -> Play {
+    };
+    let parse_play = |c: &str| -> Play {
         let parts: Vec<&str> = c.split(" ").collect();
         assert_eq!(2, parts.len());
 
         let hand = parse_hand(parts[0]);
         let bid = parts[1].parse().unwrap();
         Play { hand, bid }
-    }
+    };
 
     content
         .replace("\r\n", "\n")
@@ -99,8 +96,9 @@ fn sort_by_hand(h1: &Hand, h2: &Hand) -> Ordering {
 }
 
 fn part1_inner(content: &str) -> u64 {
-    let mut plays = parse(content);
-    println!("{:?}", plays);
+    //                          0    1    2    3    4    5    6    7    8    9   10   11   12
+    const CARDS: [char; 13] = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'];
+    let mut plays = parse(content, &CARDS);
     plays.sort_by(|p1, p2| sort_by_hand(&p1.hand, &p2.hand));
     plays.iter().enumerate()
         .fold(0, |acc, (i, play)| acc + ((i as u64 + 1)*play.bid))
@@ -112,17 +110,54 @@ pub fn part1(content: String) {
 
 // PART 2 --------------------------------------
 
+fn get_best_hand_type(h: &Hand) -> usize {
+    let jk = h.cards.iter().position(|c| *c == 0);
+    match jk {
+        None => get_hand_type(h),
+        Some(i) => {
+            let mut h2 = h.clone();
+            let mut max = get_hand_type(h);
+            for card_value in 1..13 {
+                h2.cards[i] = card_value;
+                let val = get_best_hand_type(&h2);
+                if val > max {
+                    max = val;
+                }
+            }
+            max
+        }
+    }
+}
+
+fn sort_by_hand_joker(h1: &Hand, h2: &Hand) -> Ordering {
+    let ht1 = get_best_hand_type(h1);
+    let ht2 = get_best_hand_type(h2);
+    if ht1 < ht2 {
+        Ordering::Less
+    } else if ht1 > ht2 {
+        Ordering::Greater
+    } else {
+        for (c1, c2) in h1.cards.iter().zip(h2.cards.iter()) {
+            let cmp = c1.cmp(c2);
+            if cmp != Ordering::Equal {
+                return cmp;
+            }
+        }
+        unreachable!()
+    }
+}
 
 fn part2_inner(content: &str) -> u64 {
-    let mut plays = parse(content);
-    println!("{:?}", plays);
-    plays.sort_by(|p1, p2| sort_by_hand(&p1.hand, &p2.hand));
+    //                          0    1    2    3    4    5    6    7    8    9   10   11   12
+    const CARDS: [char; 13] = ['J', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A'];
+    let mut plays = parse(content, &CARDS);
+    plays.sort_by(|p1, p2| sort_by_hand_joker(&p1.hand, &p2.hand));
     plays.iter().enumerate()
         .fold(0, |acc, (i, play)| acc + ((i as u64 + 1)*play.bid))
 }
 
 pub fn part2(content: String) {
-    println!("{}", part1_inner(&content));
+    println!("{}", part2_inner(&content));
 }
 
 
@@ -145,6 +180,6 @@ QQQJA 483
 
     #[test]
     fn part2_sample() {
-        // assert_eq!(71503, part2_inner(SAMPLE));
+        assert_eq!(5905, part2_inner(SAMPLE));
     }
 }
